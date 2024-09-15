eea0089d00b6e5e1e0d34b595da79e0b005bb3b8
Admin <blackwolf.2000@mail.ru>
2024-09-15 15:37:36 +0300

    к сервису добавь интерфейс (забыл просто про него)

--- src/main/java/com/example/PracticalTask311/service/UserService.java
+++ src/main/java/com/example/PracticalTask311/service/UserService.java
@@ -1,41 +1,16 @@
 package com.example.PracticalTask311.service;
 
 import com.example.PracticalTask311.model.User;
-import com.example.PracticalTask311.util.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
-@Service
-@Transactional(readOnly = true)
-public class UserService  {
+public interface UserService {
 
-    private final UserRepository userRepository;
+    List<User> getAllUsers();
 
-    @Autowired
-    public UserService(UserRepository userRepository) {
-        this.userRepository = userRepository;
-    }
+    void saveUser(User user);
 
+    void deleteUser(Long id);
 
-    public List<User> getAllUsers() {
-        return userRepository.findAll();
-    }
-
-    @Transactional
-    public void saveUser(User user) {
-        userRepository.save(user);
-    }
-
-    @Transactional
-    public void deleteUser(Long id) {
-        userRepository.deleteById(id);
-    }
-
-    @Transactional
-    public void updateUser(User user) {
-        userRepository.save(user);
-    }
+    void updateUser(User user);
 }
---
+++ src/main/java/com/example/PracticalTask311/service/UserServiceImpl.java
@@ -0,0 +1,40 @@
+package com.example.PracticalTask311.service;
+
+import com.example.PracticalTask311.model.User;
+import com.example.PracticalTask311.util.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+public class UserServiceImpl implements UserService {
+
+        private final UserRepository userRepository;
+
+        @Autowired
+        public UserServiceImpl(UserRepository userRepository) {
+            this.userRepository = userRepository;
+        }
+
+        public List<User> getAllUsers () {
+        return userRepository.findAll();
+    }
+
+        @Transactional
+        public void saveUser (User user){
+        userRepository.save(user);
+    }
+
+        @Transactional
+        public void deleteUser (Long id){
+        userRepository.deleteById(id);
+    }
+
+        @Transactional
+        public void updateUser (User user){
+        userRepository.save(user);
+    }
+    }
